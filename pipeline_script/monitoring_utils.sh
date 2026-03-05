5bf4f84d37e093b7672ed1de7838333362d5296d
krishnalogs <mohanbhogi05@gmail.com>
2026-03-05 15:18:22 +0530

    deployment pipelineupdated

--- pipeline_script/monitoring_utils.sh
+++ pipeline_script/monitoring_utils.sh
@@ -3,35 +3,36 @@
 set -e
 
 NAMESPACE="prometheus"
-RELEASE_NAME="kube-prometheus-stack"
+RELEASE="kube-prometheus-stack"
 
-echo "Adding Helm repositories..."
+echo "Adding Helm repo..."
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts || true
 helm repo update
 
-echo "Checking if namespace exists..."
-if kubectl get namespace $NAMESPACE > /dev/null 2>&1; then
-    echo "Namespace exists. Upgrading Helm release..."
-    helm upgrade $RELEASE_NAME prometheus-community/kube-prometheus-stack \
+echo "Checking namespace..."
+
+if kubectl get namespace $NAMESPACE >/dev/null 2>&1; then
+    echo "Namespace exists - upgrading release"
+    helm upgrade $RELEASE prometheus-community/kube-prometheus-stack \
         --namespace $NAMESPACE
 else
-    echo "Namespace does not exist. Creating namespace and installing Helm chart..."
+    echo "Namespace not found - creating and installing"
     kubectl create namespace $NAMESPACE
 
-    helm install $RELEASE_NAME prometheus-community/kube-prometheus-stack \
+    helm install $RELEASE prometheus-community/kube-prometheus-stack \
         --namespace $NAMESPACE
 fi
 
-echo "Waiting for services to be created..."
-sleep 10
+echo "Exposing Grafana and Prometheus services..."
 
-echo "Patching services to LoadBalancer..."
+kubectl patch svc ${RELEASE}-grafana \
+  -n $NAMESPACE \
+  -p '{"spec": {"type": "LoadBalancer"}}' || true
 
-kubectl patch svc ${RELEASE_NAME}-kube-prometheus-prometheus \
-  -n $NAMESPACE -p '{"spec": {"type": "LoadBalancer"}}' || true
-
-kubectl patch svc ${RELEASE_NAME}-grafana \
-  -n $NAMESPACE -p '{"spec": {"type": "LoadBalancer"}}' || true
+kubectl patch svc ${RELEASE}-kube-prometheus-prometheus \
+  -n $NAMESPACE \
+  -p '{"spec": {"type": "LoadBalancer"}}' || true
 
 echo "Deployment Completed"
+
 kubectl get svc -n $NAMESPACE
