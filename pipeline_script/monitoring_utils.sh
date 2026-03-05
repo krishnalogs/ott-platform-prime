335e0ec043310c35f05605be556ddca547e5d0b0
krishnalogs <mohanbhogi05@gmail.com>
2026-03-05 15:24:39 +0530

    deployment pipelineupdated

--- pipeline_script/monitoring_utils.sh
+++ pipeline_script/monitoring_utils.sh
@@ -5,34 +5,19 @@ set -e
 NAMESPACE="prometheus"
 RELEASE="kube-prometheus-stack"
 
-echo "Adding Helm repo..."
+echo "Adding Helm repository..."
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts || true
 helm repo update
 
-echo "Checking namespace..."
+echo "Installing / Upgrading Prometheus Stack..."
 
-if kubectl get namespace $NAMESPACE >/dev/null 2>&1; then
-    echo "Namespace exists - upgrading release"
-    helm upgrade $RELEASE prometheus-community/kube-prometheus-stack \
-        --namespace $NAMESPACE
-else
-    echo "Namespace not found - creating and installing"
-    kubectl create namespace $NAMESPACE
+helm upgrade --install $RELEASE prometheus-community/kube-prometheus-stack \
+  --namespace $NAMESPACE \
+  --create-namespace \
+  --set grafana.service.type=LoadBalancer \
+  --set prometheus.service.type=LoadBalancer
 
-    helm install $RELEASE prometheus-community/kube-prometheus-stack \
-        --namespace $NAMESPACE
-fi
+echo "Deployment completed"
 
-echo "Exposing Grafana and Prometheus services..."
-
-kubectl patch svc ${RELEASE}-grafana \
-  -n $NAMESPACE \
-  -p '{"spec": {"type": "LoadBalancer"}}' || true
-
-kubectl patch svc ${RELEASE}-kube-prometheus-prometheus \
-  -n $NAMESPACE \
-  -p '{"spec": {"type": "LoadBalancer"}}' || true
-
-echo "Deployment Completed"
-
+echo "Checking services..."
 kubectl get svc -n $NAMESPACE
