341d3aef63193cadd0d624a6f231b72c3eaa162d
krishnalogs <mohanbhogi05@gmail.com>
2026-03-05 10:49:31 +0530

    pipeline updated

--- terraform_code/eks_code/provider.tf
+++ terraform_code/eks_code/provider.tf
@@ -1,6 +1,6 @@
 locals {
   region = "ap-southeast-1"
-  name   = "amazon-prime-cluster"
+  name   = "prime-ott-platform-cluster"
   vpc_cidr = "10.0.0.0/16"
   azs      = ["ap-southeast-1a", "ap-southeast-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
